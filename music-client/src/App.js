f36bcad2e5d5a37e9eea1be2bf66a2892f37b409
T <trololol12446@gmail.com>
2022-06-01 13:56:06 -0400

    a

--- music-client/src/App.js
+++ music-client/src/App.js
@@ -4,14 +4,16 @@ import ReactDOM from "react-dom/client";
 
 import SidebarComponent from './/components/sidebar.js'
 import Albumartview from './/components/albumartview.js'
+import * from './/components/staticui.js'
 
 function App() {
   return (
     <div className="App">
-
-    <Albumartview name="asdasda" albumname="asdasd"/>
-    <Albumartview name="adssdsss"/>
-
+      <div className="staticui">
+        <div className="topbar"></div>
+        <div className="leftsidebar sidebar"></div>
+        <div className="bottombar"></div>
+      </div>
     </div>
   );
 }
--- music-client/src/components/albumartview.js
+++ music-client/src/components/albumartview.js
@@ -2,7 +2,7 @@ import './albumart.css'
 function Albumartview(props) {
   return (
 	<div className="albumartviewwrapper">
-		<img src="cover.jpg" className="albumart256"/>
+		<img src="333.jpg" className="albumart256"/>
 		<span>
 			<p className="albumartviewtext"> {props.albumname} </p> 
 			<button/>
